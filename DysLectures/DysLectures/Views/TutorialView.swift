0f6a128fcef4781805760cf509b0137eab42faa6
Antonio Pisani <pisani1997@gmail.com>
2021-11-19 11:46:23 +0100

    federica ti odio ecco la prima prova

--- DysLectures/DysLectures/Views/TutorialView.swift
+++ DysLectures/DysLectures/Views/TutorialView.swift
@@ -32,6 +32,7 @@ struct TutorialView: View {
         Text("\(page1.text2)")
           .multilineTextAlignment(.center)
           .padding()
+        Text("ti odio federica")
             } //endVStack
         } //endZStack
     } //endView
