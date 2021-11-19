3712b16676c17de983188d3c235bbd5a1d755b3e
Fcontento94 <f.contento94@gmail.com>
2021-11-19 12:27:44 +0100

    cambiate le frasi delle prime tre schermate del tutorial - prova un pull

--- DysLectures/DysLectures/Views/Data/Data.swift
+++ DysLectures/DysLectures/Views/Data/Data.swift
@@ -19,9 +19,9 @@ var pageTutorial: [Tutorial] = []
 
 var page1: Tutorial = Tutorial(text1: "Oh, I heard a strange noise…", monsterImage: "hidden-monster", text2: "It seems there is someone in that bush!")
 
-var page2: Tutorial = Tutorial(text1: "frase superiore pagina 2 tutorial", monsterImage: "hidden-monster", text2: "frase inferiore pagina 2 tutorial")
+var page2: Tutorial = Tutorial(text1: "Let me see…", monsterImage: "hidden-monster", text2: "Who is there!?")
 
-var page3: Tutorial = Tutorial(text1: "frase superiore pagina 3 tutorial", monsterImage: "hidden-monster", text2: "frase inferiore pagina 3 tutorial")
+var page3: Tutorial = Tutorial(text1: "Oh, hi! I didn’t see you…", monsterImage: "hidden-monster", text2: "I am Coco the most amazing monster in the world!")
 
 var page4: Tutorial = Tutorial(text1: "frase superiore pagina 4 tutorial", monsterImage: "hidden-monster", text2: "frase inferiore pagina 4 tutorial")
 
