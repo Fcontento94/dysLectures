64c56098fe8e4af9017e15e012d52009a3308ab2
Fcontento94 <f.contento94@gmail.com>
2021-11-20 15:39:48 +0100

    creazione di InsertNameView <3 <3 <3

--- DysLectures/DysLectures/Views/Data/DataAntonio.swift
+++ DysLectures/DysLectures/Views/Data/DataAntonio.swift
@@ -8,7 +8,9 @@
 import Foundation
 import SwiftUI
 
-let upWords = ["i Heard a strange noise...","Let me see..." ]
+let upWords = ["I Heard a strange noise...","Let me see..." ]
 let downWords = ["It seems there is someone in that bush!","Who is there!?"]
 
 
+// si può fare un array anche per le immagini del mostro?
+// vorrei fare che passiamo dal bush al monster tra la prima e la seconda scena
---
+++ DysLectures/DysLectures/Views/InsertNameView.swift
@@ -0,0 +1,64 @@
+//
+//  InsertNameView.swift
+//  DysLectures
+//
+//  Created by Federica Contento on 20/11/21.
+//
+
+import SwiftUI
+
+struct InsertNameView: View {
+    
+    @State private var insertName = ""
+    
+    
+    
+    var body: some View {
+        
+        
+        //inizio del form che contiene l'inserimento testo (TextField) e il bottoncino per confermare
+        
+
+            
+        VStack{
+                
+                VStack{
+            Text("How can I call you?")
+                .font(.custom("OpenDyslexic-Regular", size: 18))
+                .padding(.all)
+            Image("monster1")
+                .padding(.all)
+                    
+                }
+        
+        Form {
+            
+            //horizontal stack che contiene textfield e bottoncino
+            
+            HStack{
+            Section {
+                TextField("Your Name", text: $insertName)
+                    .font(.custom("OpenDyslexic-Regular", size: 18))
+            } // Fine sezione dell'inserimento testo
+                
+                // confermare il nome con questo bottone
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                    Image(systemName: "arrow.forward.circle")
+                        .foregroundColor(Color.red)
+                }
+                
+            } // fine HStack
+        } // fine del form
+        .padding(.top)
+
+        } //fine del vertical stack che contiene tutto
+    }
+    }
+
+
+
+struct InsertNameView_Previews: PreviewProvider {
+    static var previews: some View {
+        InsertNameView()
+    }
+}
