5a7af08176af77889aadc951b0a4d0bdb7b8d4e6
Antonio Pisani <pisani1997@gmail.com>
2021-11-22 12:42:49 +0100

    Inserimento della nuova view con inizio schermata gioco

---
+++ DysLectures/DysLectures/Views/Views/GameView1.swift
@@ -0,0 +1,20 @@
+//
+//  GameView1.swift
+//  DysLectures
+//
+//  Created by Antonio Pisani on 22/11/21.
+//
+
+import SwiftUI
+
+struct GameView1: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GameView1_Previews: PreviewProvider {
+    static var previews: some View {
+        GameView1()
+    }
+}
