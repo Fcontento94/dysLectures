968e2ec890fa0edb1fbf505242d1ada3d5b86f71
Fcontento94 <f.contento94@gmail.com>
2021-11-22 13:04:34 +0100

    Storia 2

--- DysLectures/DysLectures/Views/Data/Stories/Story2.swift
+++ DysLectures/DysLectures/Views/Data/Stories/Story2.swift
@@ -1,88 +1,89 @@
-////
-////  Struct-Story1.swift
-////  DysLectures
-////
-////  Created by Federica Contento on 20/11/21.
-////
 //
-//import Foundation
+//  Struct-Story1.swift
+//  DysLectures
 //
-//var little_Red_Riding_Hood: [Story] = []
+//  Created by Federica Contento on 20/11/21.
 //
-//var story1: Story = Story(
-//    
-//title: "Little Red Riding Hood",
-//    
-//bookCover: "red",
-//    
-//paragraphs: [
-//        
-////CAPITOLO 1
-//        
-////    [0]
-//               "Little Red Riding Hood lived in a wood with her mother. One day Little Red Riding Hood went to visit her granny. She had a nice cake in her basket.",
-//
-////    [1]
-//               "On her way Little Red Riding Hood met a wolf. ‘Hello!’ said the wolf: ‘Where are you going?’. Then Little Red replied: ‘I’m going to see my grandmother. She lives in a house behind those trees.’",
-////    [2]
-//               "The wolf ran to Granny’s house and ate Granny up. He got into Granny’s bed. A little  later, Little Red Riding Hood reached the house. ",
-//    
-////CAPITOLO 2
-//
-//
-////    [3]
-//               "Little Red looked at the wolf and asked: ‘Granny, what big eyes you have!’. ‘All the better to see you with!’ said the wolf.",
-////    [4]
-//               "Again Little Red asked: ‘Granny, what big ears you have!’. ‘All the better to hear you with!’ said the wolf.",
-////    [5]
-//               "‘Granny, what big teeth you have!’. ‘All the better to eat you with!’ shouted the wolf.",
-//
-////CAPITOLO 3
-//               
-////    [6]
-//               "A woodcutter was in the wood. He heard a loud scream and ran to the house.",
-//
-////    [7]
-//               "The woodcutter hit the wolf over the head. The wolf opened his mouth wide and shouted and Granny jumped out.",
-////    [8]
-//               "The wolf ran away and Little Red Riding Hood never saw the wolf again."
-//    ],
-//
-//images: [
-//    
-//    //CAPITOLO 1
-//            
-//    //    [0]
-//                   "forest",
-//
-//    //    [1]
-//                   "wolf",
-//    //    [2]
-//                   "granny",
-//        
-//    //CAPITOLO 2
-//
-//
-//    //    [3]
-//                   "wolf",
-//    //    [4]
-//                   "wolf",
-//    //    [5]
-//                   "wolf",
-//
-//    //CAPITOLO 3
-//                   
-//    //    [6]
-//                   "hunter",
-//
-//    //    [7]
-//                   "granny",
-//    //    [8]
-//                   "red"
-//]
-//    
-////    goToExercise: "Riordiniamo la storia!"
-//    
-//    
-//
-//)
+
+import Foundation
+
+var Cinderella: [Story] = []
+
+var story2: Story = Story(
+    
+title: "Cinderella",
+    
+bookCover: "shoe",
+    
+paragraphs: [
+        
+//CAPITOLO 1
+        
+//    [0]
+               "Once upon a time, in a village there lived a beautiful girl named Cinderella with her wicked stepmother and two step sisters.",
+
+//    [1]
+               "She worked hard all day. One day, they all went to a ball in the palace, leaving Cinderella behind. Cinderella was feeling sad. ",
+//    [2]
+               "Suddenly there was a burst of light and the fairy godmother appeared.",
+    
+//CAPITOLO 2
+
+
+//    [3]
+               "With a flick of the magic she turned Cinderella into a beautiful princess.",
+//    [4]
+               "with glass slippers and a horse carriage appeared at the door.",
+//    [5]
+               "The fairy godmother warned Cinderella to return before midnight. Cinderella arrived at the ball, the prince saw her and fell in love with her. ",
+
+//CAPITOLO 3
+               
+//    [6]
+               "They danced together all night. as the clock struck twelve, Cinderella rushed out to her carriage leaving one of her slippers behind.",
+
+//    [7]
+               "The prince went to every house in the town with the slipper until he found Cinderella.",
+//    [8]
+               "The prince and Cinderella lived happily ever after."
+    ],
+
+images: [
+    
+    //CAPITOLO 1
+            
+    //    [0]
+                   "cinderella-sad",
+
+    //    [1]
+                   "castle",
+    //    [2]
+                   "fairy",
+        
+    //CAPITOLO 2
+
+
+    //    [3]
+                   "princess",
+    //    [4]
+                   "pumpkin-carriage",
+    //    [5]
+                   "prince",
+
+    //CAPITOLO 3
+                   
+    //    [6]
+                   "shoe",
+
+    //    [7]
+                   "prince",
+    //    [8]
+                   "princess"
+        
+        ]
+    
+//    goToExercise: "Riordiniamo la storia!"
+    
+    
+
+)
---
+++ DysLectures/DysLectures/Views/Views/Reading_Views/ReadingView2.swift
@@ -0,0 +1,190 @@
+//
+//  ReadingView.swift
+//  DysLectures
+//
+//  Created by Federica Contento on 19/11/21.
+//
+
+import Foundation
+import SwiftUI
+
+struct ReadingView2: View {
+    
+    var body: some View {
+        
+        ZStack {
+            Image("dyslexic-ruler")
+                .renderingMode(.original)
+                .resizable(resizingMode: .stretch)
+                .aspectRatio(contentMode: .fill)
+                .padding(.bottom)
+            //immagine del dyslexic ruler
+
+            // INIZIO STORIA
+        VStack{
+        
+            Text("\(story2.title)")
+                .foregroundColor(Color(red: 0.764, green: 0.143, blue: 0.186))
+                .multilineTextAlignment(.center)
+                .padding(.top, 50.0)
+                .font(.custom("OpenDyslexic-Bold", size: 25))
+            
+           
+            
+        ScrollView(.vertical, showsIndicators: true) {
+
+            Group {
+
+Text("Chapter 1")
+.multilineTextAlignment(.center)
+.padding(.top, 1.0)
+.font(.custom("OpenDyslexic-Bold", size: 25))
+            
+            Image("\(story2.images[0])")
+                .padding(.all)
+                .frame(width: 300.0)
+                .border(/*@START_MENU_TOKEN@*/Color.red/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/5/*@END_MENU_TOKEN@*/)
+
+
+            Text("\(story2.paragraphs[0])")
+                .multilineTextAlignment(.leading)
+                .padding(.top)
+                .font(.custom("OpenDyslexic-Regular", size: 25))
+            
+            Image("\(story2.images[1])")
+                .padding(.all)
+                .frame(width: 300.0)
+                .border(/*@START_MENU_TOKEN@*/Color.red/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/5/*@END_MENU_TOKEN@*/)
+
+
+            Text("\(story2.paragraphs[1])")
+                .multilineTextAlignment(.leading)
+                .padding(.top)
+                .font(.custom("OpenDyslexic-Regular", size: 25))
+            
+            Image("\(story2.images[2])")
+                .padding(.all)
+                .frame(width: 300.0)
+                .border(/*@START_MENU_TOKEN@*/Color.red/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/5/*@END_MENU_TOKEN@*/)
+
+
+            Text("\(story2.paragraphs[2])")
+                .multilineTextAlignment(.leading)
+                .padding(.top)
+                .font(.custom("OpenDyslexic-Regular", size: 25))
+            }
+            
+
+Group {
+            
+Text("Chapter 2")
+.multilineTextAlignment(.center)
+.padding(.top, 1.0)
+.font(.custom("OpenDyslexic-Bold", size: 25))
+                        
+                        Image("\(story2.images[3])")
+                            .padding(.all)
+                            .frame(width: 300.0)
+                            .border(/*@START_MENU_TOKEN@*/Color.red/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/5/*@END_MENU_TOKEN@*/)
+
+
+                        Text("\(story2.paragraphs[3])")
+                            .multilineTextAlignment(.leading)
+                            .padding(.top)
+                            .font(.custom("OpenDyslexic-Regular", size: 25))
+                        
+                        Image("\(story2.images[4])")
+                            .padding(.all)
+                            .frame(width: 300.0)
+                            .border(/*@START_MENU_TOKEN@*/Color.red/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/5/*@END_MENU_TOKEN@*/)
+
+
+                        Text("\(story2.paragraphs[4])")
+                            .multilineTextAlignment(.leading)
+                            .padding(.top)
+                            .font(.custom("OpenDyslexic-Regular", size: 25))
+                        
+                        Image("\(story2.images[5])")
+                            .padding(.all)
+                            .frame(width: 300.0)
+                            .border(/*@START_MENU_TOKEN@*/Color.red/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/5/*@END_MENU_TOKEN@*/)
+
+
+                        Text("\(story2.paragraphs[5])")
+                            .multilineTextAlignment(.leading)
+                            .padding(.top)
+                            .font(.custom("OpenDyslexic-Regular", size: 25))
+            }
+            
+
+Group {
+                        
+Text("Chapter 3")
+.multilineTextAlignment(.center)
+.padding(.top, 1.0)
+.font(.custom("OpenDyslexic-Bold", size: 25))
+                                    
+                           Image("\(story2.images[6])")
+                                .padding(.all)
+                                .frame(width: 300.0)
+                                        .border(/*@START_MENU_TOKEN@*/Color.red/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/5/*@END_MENU_TOKEN@*/)
+
+
+                                    Text("\(story2.paragraphs[6])")
+                                        .multilineTextAlignment(.leading)
+                                        .padding(.top)
+                                        .font(.custom("OpenDyslexic-Regular", size: 25))
+                                    
+                                    Image("\(story2.images[7])")
+                                        .padding(.all)
+                                        .frame(width: 300.0)
+                                        .border(/*@START_MENU_TOKEN@*/Color.red/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/5/*@END_MENU_TOKEN@*/)
+
+
+                                    Text("\(story2.paragraphs[7])")
+                                        .multilineTextAlignment(.leading)
+                                        .padding(.top)
+                                        .font(.custom("OpenDyslexic-Regular", size: 25))
+                                    
+                                    Image("\(story2.images[8])")
+                                        .padding(.all)
+                                        .frame(width: 300.0)
+                                        .border(/*@START_MENU_TOKEN@*/Color.red/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/5/*@END_MENU_TOKEN@*/)
+
+
+                                    Text("\(story2.paragraphs[8])")
+                                        .multilineTextAlignment(.leading)
+                                        .padding(.top)
+                                        .font(.custom("OpenDyslexic-Regular", size: 25))
+                        }
+
+Button(action:{
+                
+}, label: {
+    Text("Go to Play!")
+        .padding()
+        .font(.custom("OpenDyslexic-Bold", size: 20))
+     
+        .foregroundColor(Color.white)
+        .frame(width: 200, height: 60)
+        .background(Color(hue: 0.63, saturation: 0.734, brightness: 1.0))
+        .cornerRadius(20)
+
+
+})
+.padding(.bottom, 115.0)
+            
+                } //FINE STORIA > fine vstack
+        }
+        .padding(.horizontal)
+        }
+        }
+    }
+
+
+struct ReadingView2_Previews: PreviewProvider {
+    static var previews: some View {
+        ReadingView2()
+    }
+}
+
